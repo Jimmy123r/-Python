1f6b463973f9d075c2372ada9ae6c13d0b9c717d
Jimmy123r <90005667+Jimmy123r@users.noreply.github.com>
2021-09-15 21:41:35 -0300

    Create basico.py

---
+++ basico.py
@@ -0,0 +1,26 @@
+"""
+funcion input ()
+       casting o conversion de datos
+    concatenacion
+    condicionales
+         if - else
+"""
+
+"""
+nombre = input ("ingresa un nombre :")
+apellido = input ("ingresa un apellido :")
+colegio = input (" ingrese colegio : ")
+
+print(nombre)
+print(apellido)
+print(colegio)
+print (nombre + " " + apellido + " "  + colegio )
+print (f ": {nombre}  {apellido} -  {colegio} ")
+
+"""
+
+#int() - covierte a enteros
+
+#float() - convierte a  decimales
+numero = int(input("ingrese el numero : "))
+print (numero + 10)
