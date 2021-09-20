f7195f703bc3104eeced8bf2c0b3b683bf4d6226
Jimmy123r <90005667+Jimmy123r@users.noreply.github.com>
2021-09-20 09:15:50 -0300

    Update trabajo1.py

--- trabajo1.py
+++ trabajo1.py
@@ -1,11 +1,17 @@
 print ("bienvenida")
 numero1 = int(input("ingresar primer numero : "))
 numero2 = int(input("ingresar segundo numero : "))
+If número2 == 0 :
+   Print("error")
+Else :
+    print("resultado de la suma :", numero1 + numero2)
+    print("resultado de la resta :", numero1 - numero2)
+    print("resultado de la multiplicacion :", numero1 * numero2)
+    print("resultado de la potenciacion :", numero1 ** numero2)
+    print("resultado de la division :", numero1 / numero2)
+    print("resultado de la  division entera :", numero1 // numero2)
+    print("resultado del modulo :", numero1 % numero2)
 
-print("resultado de la suma :", numero1 + numero2)
-print("resultado de la resta :", numero1 - numero2)
-print("resultado de la multiplicacion :", numero1 * numero2)
-print("resultado de la potenciacion :", numero1 ** numero2)
-print("resultado de la division :", numero1 / numero2)
-print("resultado de la  division entera :", numero1 // numero2)
-print("resultado del modulo :", numero1 % numero2)
+    
+
+
